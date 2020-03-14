34add6b0bbe685382e671fa1c3336b0d8025f2c3
t-makino <t-makino@atware.co.jp>
2020-03-14 16:01:30 +0900

    feature/#11 add api for review

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-from application.controllers import brand_controller
+from application.controllers import brand_controller, review_controller
 
 
 app = Flask(__name__)
 app.register_blueprint(brand_controller.app, url_prefix='/api')
-
+app.register_blueprint(review_controller.app, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
