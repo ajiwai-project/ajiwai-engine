22bfd2e31ad0320446b954fe48bcb7a7a316aadf
makino <b1013059@gmail.com>
2020-03-29 12:11:42 +0900

    feature/initialize-firestore arrange initialize store process.

--- infrastructure/repositories/brand_repository.py
+++ infrastructure/repositories/brand_repository.py
@@ -3,20 +3,12 @@ from infrastructure.dao.brand_dao import BrandDao
 
 brands_ref = db.collection('brands')
 
+
 class BrandRepository:
     def __init__(self):
         self.brand_dao = BrandDao()
 
-    def create(self, brand_id):
-        return brands_ref.add(brand.parse())
-
-
     def create(self, name, prefecture, maker, image_url):
-        brand = self.brand_dao.find_by_user_id(brand.brand_id)
-
-        if brand is None:
-            return False
-        
-        tmp = self.brand_dao.register(name, prefecture, maker, image_url)
-        print(tmp)
-
+        doc = self.brand_dao.register(name, prefecture, maker, image_url)
+        doc_snap = doc[1].get()
+        return doc_snap.id
